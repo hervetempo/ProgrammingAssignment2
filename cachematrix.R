e9e70a235c64dd39724515da9aaf601d16f082de
hervetempo <hervetempo@users.noreply.github.com>
2015-05-14 13:35:04 +0200

    Update cachematrix.R
    result programming assingment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+                    #######################
+                    #    Inverse Matrix   #
+                    #######################
 
-## Write a short comment describing this function
+## This functions inverse square matrix an cache result in an environment which is different from the current environment
+
+## The first fonction "makeCacheMatrix" make tree actions
+# 1. It test if x is a matrix
+# 2. It test if x is a square matrix
+# 3. It test if x is inversible
+                    
 
 makeCacheMatrix <- function(x = matrix()) {
+ 
+        if(class(x)!="matrix"){
+          message("'x' must be class of matrix")
+        } else {
+                if(nrow(x)!=ncol(x)){
+                  message("'x' must be square matrix")
+                } else {
+                          if(det(x)==0){
+                          message("'x' is not inversible")
+                        } else {
+                                m <- NULL
+                                set <- function(y) {
+                                  x <<- y
+                                  m <<- NULL
+                                  }
+                                  get <- function() x
+                                  setsolve <- function(solve) m <<- solve
+                                  getsolve <- function() m
+                                  list(set = set, get = get,
+                                       setsolve = setsolve,
+                                       getsolve = getsolve)
+                                 }
+                        }
+                }
+     }
 
-}
 
-
-## Write a short comment describing this function
+## The second fonction "cacheSolve" make two actions
+# 1. it first checks to see if inverse of matrix  has already been calculated. If so, it gets result from the cache and skips the computation.
+# 2. if inverse has not yet calculate, it inverse the data and sets the value the cache via the setsolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+          m <- x$getsolve()
+          if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+                     }
+                      data <- x$get()
+                      m <- solve(data, ...)
+                      x$setsolve(m)
+                      m
+    }
